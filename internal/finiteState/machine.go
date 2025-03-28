e79f3e602759153c93917bf133773e48197085fb
RT <robbyt@robbyt.net>
2025-03-27 21:39:02 -0400

    consolidate status constants constructor for the local FSM abstraction layer

--- internal/finiteState/machine.go
+++ internal/finiteState/machine.go
@@ -2,7 +2,24 @@ package finiteState
 
 import (
 	"context"
+	"log/slog"
+
+	"github.com/robbyt/go-fsm"
 )
+
+const (
+	StatusNew       = fsm.StatusNew
+	StatusBooting   = fsm.StatusBooting
+	StatusRunning   = fsm.StatusRunning
+	StatusReloading = fsm.StatusReloading
+	StatusStopping  = fsm.StatusStopping
+	StatusStopped   = fsm.StatusStopped
+	StatusError     = fsm.StatusError
+	StatusUnknown   = fsm.StatusUnknown
+)
+
+// TypicalTransitions is a set of standard transitions for a finite state machine.
+var TypicalTransitions = fsm.TypicalTransitions
 
 // Machine defines the interface for the finite state machine that tracks
 // the HTTP server's lifecycle states. This abstraction allows for different
@@ -26,4 +43,9 @@ type Machine interface {
 	// GetStateChan returns a channel that emits the state machine's state whenever it changes.
 	// The channel is closed when the provided context is canceled.
 	GetStateChan(ctx context.Context) <-chan string
+}
+
+// New creates a new finite state machine with the specified logger using "standard" state transitions.
+func New(handler slog.Handler) (*fsm.Machine, error) {
+	return fsm.New(handler, StatusNew, TypicalTransitions)
 }
